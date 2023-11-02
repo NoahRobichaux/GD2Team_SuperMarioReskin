ed5ee37d3ef8067fe49e60ddd714ae81fb00c4df
NoahRobichaux <117842535+NoahRobichaux@users.noreply.github.com>
2023-11-01 21:23:23 -0400

    idek man
    im going to bed

---
+++ Assets/Scripts/SpongeDollars.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpongeDollars : MonoBehaviour
+{
+    public GameObject questionBlock;
+    public GameObject questionBlock1;
+    public GameObject questionBlock2;
+    public GameObject questionBlock3;
+    public GameObject questionBlock4;
+    public GameObject questionBlock5;
+    public GameObject questionBlock6;
+    public GameObject questionBlock7;
+    public GameObject questionBlock8;
+    public GameObject questionBlock9;
+    public GameObject questionBlock10;
+    public GameObject questionBlock11;
+
+    public Rigidbody2D questionBlockRB;
+    public Rigidbody2D questionBlockRB1;
+    public Rigidbody2D questionBlockRB2;
+    public Rigidbody2D questionBlockRB3;
+    public Rigidbody2D questionBlockRB4;
+    public Rigidbody2D questionBlockRB5;
+    public Rigidbody2D questionBlockRB6;
+    public Rigidbody2D questionBlockRB7;
+    public Rigidbody2D questionBlockRB8;
+    public Rigidbody2D questionBlockRB9;
+    public Rigidbody2D questionBlockRB10;
+    public Rigidbody2D questionBlockRB11;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        questionBlockRB = GetComponent<Rigidbody2D>();
+        questionBlockRB1 = GetComponent<Rigidbody2D>();
+        questionBlockRB2 = GetComponent<Rigidbody2D>();
+        questionBlockRB3 = GetComponent<Rigidbody2D>();
+        questionBlockRB3 = GetComponent<Rigidbody2D>();
+        questionBlockRB3 = GetComponent<Rigidbody2D>();
+        questionBlockRB3 = GetComponent<Rigidbody2D>();
+        questionBlockRB3 = GetComponent<Rigidbody2D>();
+        questionBlockRB3 = GetComponent<Rigidbody2D>();
+        questionBlockRB3 = GetComponent<Rigidbody2D>();
+        questionBlockRB3 = GetComponent<Rigidbody2D>();
+        questionBlockRB3 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
