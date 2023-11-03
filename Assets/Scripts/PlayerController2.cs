ff03f47bbb00b368649bf7a72aa65c82aae12f18
NoahRobichaux <117842535+NoahRobichaux@users.noreply.github.com>
2023-11-03 16:36:31 -0400

    working coin counter
    i broke it when trying to add a new mechanic

---
+++ Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "QuestionBlock")
+        {
+            SpongeDollars.isBlockHit = true;
+        }
+
+        if (collision.gameObject.name == "QuestionBlock (1)")
+        {
+            SpongeDollars.isBlock1Hit = true;
+        }
+
+        if (collision.gameObject.name == "QuestionBlock (2)")
+        {
+            SpongeDollars.isBlock2Hit = true;
+        }
+
+        if (collision.gameObject.name == "QuestionBlock (3)")
+        {
+            SpongeDollars.isBlock3Hit = true;
+        }
+
+        if (collision.gameObject.name == "QuestionBlock (4)")
+        {
+            SpongeDollars.isBlock4Hit = true;
+        }
+
+        if (collision.gameObject.name == "QuestionBlock (5)")
+        {
+            SpongeDollars.isBlock5Hit = true;
+        }
+
+        if (collision.gameObject.name == "QuestionBlock (6)")
+        {
+            SpongeDollars.isBlock6Hit = true;
+        }
+
+        if (collision.gameObject.name == "QuestionBlock (7)")
+        {
+            SpongeDollars.isBlock7Hit = true;
+        }
+
+        if (collision.gameObject.name == "QuestionBlock (8)")
+        {
+            SpongeDollars.isBlock8Hit = true;
+        }
+
+        if (collision.gameObject.name == "QuestionBlock (9)")
+        {
+            SpongeDollars.isBlock9Hit = true;
+        }
+
+        if (collision.gameObject.name == "QuestionBlock (10)")
+        {
+            SpongeDollars.isBlock10Hit = true;
+        }
+
+        if (collision.gameObject.name == "QuestionBlock (11)")
+        {
+            SpongeDollars.isBlock11Hit = true;
+        }
+    }
+}
--- Assets/Scripts/SpongeDollars.cs
+++ Assets/Scripts/SpongeDollars.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SpongeDollars : MonoBehaviour
@@ -30,6 +31,25 @@ public class SpongeDollars : MonoBehaviour
     public Rigidbody2D questionBlockRB10;
     public Rigidbody2D questionBlockRB11;
 
+    public TMP_Text scoreText;
+    public TMP_Text coinCounter;
+
+    public int score = 0;
+    public int coinCount = 0;
+
+    public static bool isBlockHit = false;
+    public static bool isBlock1Hit = false;
+    public static bool isBlock2Hit = false;
+    public static bool isBlock3Hit = false;
+    public static bool isBlock4Hit = false;
+    public static bool isBlock5Hit = false;
+    public static bool isBlock6Hit = false;
+    public static bool isBlock7Hit = false;
+    public static bool isBlock8Hit = false;
+    public static bool isBlock9Hit = false;
+    public static bool isBlock10Hit = false;
+    public static bool isBlock11Hit = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,19 +57,118 @@ public class SpongeDollars : MonoBehaviour
         questionBlockRB1 = GetComponent<Rigidbody2D>();
         questionBlockRB2 = GetComponent<Rigidbody2D>();
         questionBlockRB3 = GetComponent<Rigidbody2D>();
-        questionBlockRB3 = GetComponent<Rigidbody2D>();
-        questionBlockRB3 = GetComponent<Rigidbody2D>();
-        questionBlockRB3 = GetComponent<Rigidbody2D>();
-        questionBlockRB3 = GetComponent<Rigidbody2D>();
-        questionBlockRB3 = GetComponent<Rigidbody2D>();
-        questionBlockRB3 = GetComponent<Rigidbody2D>();
-        questionBlockRB3 = GetComponent<Rigidbody2D>();
-        questionBlockRB3 = GetComponent<Rigidbody2D>();
+        questionBlockRB4 = GetComponent<Rigidbody2D>();
+        questionBlockRB5 = GetComponent<Rigidbody2D>();
+        questionBlockRB6 = GetComponent<Rigidbody2D>();
+        questionBlockRB7 = GetComponent<Rigidbody2D>();
+        questionBlockRB8 = GetComponent<Rigidbody2D>();
+        questionBlockRB9 = GetComponent<Rigidbody2D>();
+        questionBlockRB10 = GetComponent<Rigidbody2D>();
+        questionBlockRB11 = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && isBlockHit == false)
+        {
+            score += 100;
+            coinCount += 1;
+            scoreText.SetText("" + score);
+            coinCounter.SetText("x " + coinCount);
+        }
+
+        if (collision.gameObject.tag == "Player" && isBlock1Hit == false)
+        {
+            score += 100;
+            coinCount += 1;
+            scoreText.SetText("" + score);
+            coinCounter.SetText("x " + coinCount);
+        }
+
+        if (collision.gameObject.tag == "Player" && isBlock2Hit == false)
+        {
+            score += 100;
+            coinCount += 1;
+            scoreText.SetText("" + score);
+            coinCounter.SetText("x " + coinCount);
+        }
+
+        if (collision.gameObject.tag == "Player" && isBlock3Hit == false)
+        {
+            score += 100;
+            coinCount += 1;
+            scoreText.SetText("" + score);
+            coinCounter.SetText("x " + coinCount);
+        }
+
+        if (collision.gameObject.tag == "Player" && isBlock4Hit == false)
+        {
+            score += 100;
+            coinCount += 1;
+            scoreText.SetText("" + score);
+            coinCounter.SetText("x " + coinCount);
+        }
+
+        if (collision.gameObject.tag == "Player" && isBlock5Hit == false)
+        {
+            score += 100;
+            coinCount += 1;
+            scoreText.SetText("" + score);
+            coinCounter.SetText("x " + coinCount);
+        }
+
+        if (collision.gameObject.tag == "Player" && isBlock6Hit == false)
+        {
+            score += 100;
+            coinCount += 1;
+            scoreText.SetText("" + score);
+            coinCounter.SetText("x " + coinCount);
+        }
+
+        if (collision.gameObject.tag == "Player" && isBlock7Hit == false)
+        {
+            score += 100;
+            coinCount += 1;
+            scoreText.SetText("" + score);
+            coinCounter.SetText("x " + coinCount);
+        }
+
+        if (collision.gameObject.tag == "Player" && isBlock8Hit == false)
+        {
+            score += 100;
+            coinCount += 1;
+            scoreText.SetText("" + score);
+            coinCounter.SetText("x " + coinCount);
+        }
+
+        if (collision.gameObject.tag == "Player" && isBlock9Hit == false)
+        {
+            score += 100;
+            coinCount += 1;
+            scoreText.SetText("" + score);
+            coinCounter.SetText("x " + coinCount);
+        }
+
+        if (collision.gameObject.tag == "Player" && isBlock10Hit == false)
+        {
+            score += 100;
+            coinCount += 1;
+            scoreText.SetText("" + score);
+            coinCounter.SetText("x " + coinCount);
+        }
+
+        if (collision.gameObject.tag == "Player" && isBlock11Hit == false)
+        {
+            score += 100;
+            coinCount += 1;
+            scoreText.SetText("" + score);
+            coinCounter.SetText("x " + coinCount);
+        }
     }
 }
